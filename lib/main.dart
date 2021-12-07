768fec59b6f20f2ef024e8d8fa431719f243c8d8
laila.nabil <laila.n.mostafa@gmail.com>
2021-12-08 00:02:33 +0200

    adding background images

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
 import './screens/login_screen.dart';
+import 'models/customMaterialColor.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: CustomMaterialColor.generateCustomColor(35,21,84),
       ),
       home: LoginScreen(),
     );
