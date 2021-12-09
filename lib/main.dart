f946c423957cbf1b6dc53b5fe0617e45083572e7
laila.nabil <laila.n.mostafa@gmail.com>
2021-12-09 20:31:50 +0200

    profile screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:login_screen/screens/profile_screen.dart';
 
 import './screens/login_screen.dart';
 import 'helper/customMaterialColor.dart';
@@ -25,7 +26,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: CustomMaterialColor.generateCustomColor(35,21,84),
       ),
-      home: LoginScreen(),
+      // home: LoginScreen(),
+      home: ProfileScreen(),
     );
   }
 }
